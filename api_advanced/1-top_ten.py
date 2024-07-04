abf08a75eb5156f6f4dac7229b7af0dcb2d35c5e
Hirw23 <w.hirwa@alustudent.com>
2024-07-03 21:30:47 -0700

    print top 10 hot posts

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,24 +1,13 @@
 #!/usr/bin/python3
-
 """
-This module contains a function that queries the Reddit API and
-returns the top 10 hot posts for a given subreddit.
+Contains the top_ten function
 """
 
 import requests
 
+
 def top_ten(subreddit):
-    """
-    Queries the Reddit API and prints the titles of
-    the top 10 hot posts for a given subreddit.
-    
-    Args:
-        subreddit (str): The name of the subreddit to query.
-    
-    Returns:
-        None: Prints the titles of the top 10 hot posts or
-        None if the subreddit is invalid.
-    """
+    """prints the titles of the top ten hot posts for a given subreddit"""
     if subreddit is None or type(subreddit) is not str:
         print(None)
     r = requests.get('http://www.reddit.com/r/{}/hot.json'.format(subreddit),
