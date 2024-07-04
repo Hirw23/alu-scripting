a099bee489b78fc81417f09f2d083ce02e4d1e7e
Hirw23 <w.hirwa@alustudent.com>
2024-07-03 21:14:21 -0700

    subreddit subs

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,17 +1,26 @@
 #!/usr/bin/python3
-"""
-Contains the number_of_subscribers function
-"""
+# -*- coding: utf-8 -*-
+
+""" Returns the number of subscribers for a given subreddit. """
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers for a given subreddit"""
-    if subreddit is None or type(subreddit) is not str:
+    """ Returns the number of subscribers for a given subreddit. """
+
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = \
+        {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)\
+          AppleWebKit/537.36(KHTML, like Gecko) \
+         Chrome/90.0.4430.93 Safari/537.36'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        return response.json().get('data').get('subscribers')
+    else:
         return 0
-    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
-                     headers={'User-Agent': '0x16-api_advanced:project:\
-v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
-    subs = r.get("data", {}).get("subscribers", 0)
-    return subs
+
+
+if __name__ == '__main__':
+    print(number_of_subscribers)('programming')
+    print(number_of_subscribers)('not_a_valid_subreddit')
