f808ee4699c44655c6ecfafa92cdf7e0eef0fdf2
Hirw23 <w.hirwa@alustudent.com>
2024-07-06 10:38:34 -0700

    function that queries the Reddit API and returns the number of subscribers

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,26 +1,19 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
-
-""" Returns the number of subscribers for a given subreddit. """
+"""
+Script that queries subscribers on a given Reddit subreddit.
+"""
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """ Returns the number of subscribers for a given subreddit. """
-
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = \
-        {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)\
-          AppleWebKit/537.36(KHTML, like Gecko) \
-         Chrome/90.0.4430.93 Safari/537.36'}
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
-        return response.json().get('data').get('subscribers')
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
     else:
         return 0
-
-
-if __name__ == '__main__':
-    print(number_of_subscribers)('programming')
-    print(number_of_subscribers)('not_a_valid_subreddit')
